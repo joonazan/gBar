bd394855b2bef2332c37574947caac01cf5c8075
= <joon.saar@gmail.com>
2023-02-21 19:39:07 +0200

    package for NixOS

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "gBar status bar for Wayland including volume control etc.";
+
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.default =
+      with import nixpkgs { system = "x86_64-linux"; };
+      stdenv.mkDerivation {
+        name = "gBar";
+        src = ./.;
+        nativeBuildInputs = [meson ninja pkg-config];
+        buildInputs = [gtk3 gtk-layer-shell libpulseaudio];
+        build = ''
+          meson build --buildtype=release -DWithHyprland=false -DWithNvidia=false -DWithBlueZ=false
+        '';
+      };
+  };
+}
